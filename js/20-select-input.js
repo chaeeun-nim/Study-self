25bfd38cedc22c230ea50c12af97c39ed12dbb6d
chaeeun-nim <qlqldkdk0@gamil.com>
2025-04-11 19:47:48 +0900

    ✨ 반복문에 관한 학습

--- js/20-select-input.js
+++ js/20-select-input.js
@@ -14,21 +14,31 @@
 
 let pro = document.querySelectorAll(".form-select")[0];
 let option = document.querySelectorAll(".form-select")[1];
-const Option = [
-  `<option>95</option><option>100</option>`,
-  `<option>29</option><option>28</option>`,
-];
+
+const pants = [28, 30, 32];
+const shrits = [95, 100, 105];
 
 pro.addEventListener("change", function () {
   let val = this.value;
 
   if (val == "shirts") {
     option.classList.remove("dis-none");
-    option.innerHTML = Option[0];
+    option.innerHTML = "";
+    shrits.forEach(function (a) {
+      option.innerHTML += `<option>${a}</option>`;
+    });
   } else if (val == "pants") {
     option.classList.remove("dis-none");
-    option.innerHTML = Option[1];
+    option.innerHTML = "";
+    pants.forEach((a) => {
+      option.innerHTML += `<option>${a}</option>`;
+    });
   } else {
     option.classList.add("dis-none");
   }
 });
+
+var obj = { name: "kim", age: 20 };
+for (let i in obj) {
+  console.log(obj[i]);
+}
