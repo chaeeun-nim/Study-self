065e8b849040fe21e62b0752934dab2a1768aea2
chaeeun-nim <qlqldkdk0@gamil.com>
2025-04-23 17:10:32 +0900

    ✨ 더보기버튼, 가격순 정렬버튼

--- js/23-goodslist.js
+++ js/23-goodslist.js
@@ -3,6 +3,40 @@ var products = [
   { id: 1, price: 50000, title: "Springfield Shirt" },
   { id: 2, price: 60000, title: "Black Monastery" },
 ];
+// products.sort((a, b) => {
+//   return a.price - b.price;
+// });
+// console.log(products);
+
+const price = document.querySelector("#price");
+price.addEventListener("click", () => {
+  products.sort((a, b) => {
+    return a.price - b.price;
+  });
+  document.querySelector(".row").innerHTML = "";
+  cardList(products);
+});
+
+// 상품명 다나가 순 정렬
+const maker = document.querySelector("#maker");
+
+// 6만원 이하 상품만 보기
+const cheap = document.querySelector("#cheap");
+
+var 어레이 = [7, 3, 5, 2, 40];
+// 어레이.sort((a, b) => {
+//   return a - b;
+// });
+var 새어레이 = 어레이.map(function (a) {
+  return a * 3;
+});
+console.log(새어레이);
+
+// var 어레이2 = ["a", "c", "b", "d"];
+// 어레이2.sort((a, b) => {
+//   return a > b ? -1 : 1;
+// });
+// console.log(어레이2);
 
 /*
 <div class="col-sm-4">
@@ -41,6 +75,8 @@ function makeCard(dataArr) {
   divElem.appendChild(h5Elem);
   divElem.appendChild(pElem);
   targetDiv.appendChild(divElem);
+
+  return divElem;
 }
 
 function dataGet(link) {
